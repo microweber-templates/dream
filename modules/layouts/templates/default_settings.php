95253129d05c4737e51c0167a0a221ac1453b71e
Peter Ivanov <peter@microweber.com>
2019-05-08 13:15:32 +0300

    update

---
+++ modules/layouts/templates/default_settings.php
@@ -0,0 +1,6 @@
+<div id="settings-holder">
+    <br/><br/>
+    <h5 style="font-weight: bold; display: block;"><?php _lang("Skin Settings", "templates/dream"); ?></h5>
+
+    <?php  include __DIR__.'/settings_padding.php'; ?>
+</div>
---
+++ modules/layouts/templates/settings_padding_front.php
@@ -0,0 +1,16 @@
+
+<?php
+if (!$classes['padding_top']) {
+    $classes['padding_top'] = '';
+}
+if (!$classes['padding_bottom']) {
+    $classes['padding_bottom'] = '';
+}
+
+$layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom'] . ' ';
+?>
+
+<?php
+/* Padding */
+$padding = $layout_classes;
+?>
