bdbcbcc114621634ea2ad1a23cd394a97ad6491c
Peter Ivanov <peter@microweber.com>
2020-02-18 16:04:06 +0200

    Incrementing version number to 3.5

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = array();
 $config['name'] = "Dream";
 $config['author'] = "Microweber";
-$config['version'] = 3.4;
+$config['version'] = 3.5;
 $config['url'] = "http://microweber.com";
 $config['standalone_module_skins'] = true;
 $config['is_default'] = 1; //if you set this parameter the template will be selected on the install screen
