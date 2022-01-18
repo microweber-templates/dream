1a6f071359619fd996b5623f66fd48640f918e2a
Nikolay <rachkuw@gmail.com>
2022-01-18 18:26:48 +0200

    update

---
+++ layouts/contact_us.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+type: layout
+content_type: static
+name: Contact Us
+position: 6
+description: Contact Us
+
+*/
+
+
+?>
+<?php include template_dir() . "header.php"; ?>
+
+    <div class="edit main-content" data-layout-container rel="content" field="content">
+        <module type="layouts" template="titles/skin-4"/>
+        <module type="layouts" template="contacts/skin-16"/>
+    </div>
+
+<?php include template_dir() . "footer.php"; ?>
